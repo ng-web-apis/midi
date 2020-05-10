142e366f17b19fb4e7ca67b025d01500cdc851fa
waterplea <alexander@inkin.ru>
2020-05-10 13:31:46 +0300

    proper resubscribe

--- projects/midi/src/tokens/midi-messages.ts
+++ projects/midi/src/tokens/midi-messages.ts
@@ -1,7 +1,7 @@
 import {inject, InjectionToken} from '@angular/core';
 import {from, fromEvent, merge, Observable, throwError} from 'rxjs';
 import {FromEventTarget} from 'rxjs/internal/observable/fromEvent';
-import {repeat, share, switchMap, takeUntil} from 'rxjs/operators';
+import {share, startWith, switchMap} from 'rxjs/operators';
 import {MIDI_ACCESS} from './midi-access';
 
 import MIDIMessageEvent = WebMidi.MIDIMessageEvent;
@@ -16,21 +16,21 @@ export const MIDI_MESSAGES = new InjectionToken<Observable<MIDIMessageEvent>>(
                 switchMap(access =>
                     access instanceof Error
                         ? throwError(access)
-                        : merge(
-                              ...Array.from(access.inputs).map(([_, input]) =>
-                                  fromEvent(
-                                      input as FromEventTarget<MIDIMessageEvent>,
-                                      'midimessage',
-                                  ),
-                              ),
+                        : fromEvent(
+                              access as FromEventTarget<MIDIConnectionEvent>,
+                              'statechange',
                           ).pipe(
-                              takeUntil(
-                                  fromEvent(
-                                      access as FromEventTarget<MIDIConnectionEvent>,
-                                      'statechange',
+                              startWith(null),
+                              switchMap(() =>
+                                  merge(
+                                      ...Array.from(access.inputs).map(([_, input]) =>
+                                          fromEvent(
+                                              input as FromEventTarget<MIDIMessageEvent>,
+                                              'midimessage',
+                                          ),
+                                      ),
                                   ),
                               ),
-                              repeat(),
                           ),
                 ),
                 share(),
