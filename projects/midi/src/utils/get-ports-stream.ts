d293b55cfb94952f6d212a2af59ece31f001b4a0
waterplea <alexander@inkin.ru>
2020-05-14 23:19:58 +0300

    feat(tokens): add MIDI_INPUTS and MIDI_OUTPUTS

---
+++ projects/midi/src/utils/get-ports-stream.ts
@@ -0,0 +1,26 @@
+import MIDIInput = WebMidi.MIDIInput;
+import MIDIOutput = WebMidi.MIDIOutput;
+
+import {inject} from '@angular/core';
+import {MIDI_ACCESS} from '@ng-web-apis/midi';
+import {from, fromEvent, Observable, of} from 'rxjs';
+import {FromEventTarget} from 'rxjs/internal/observable/fromEvent';
+import {map, shareReplay, startWith, switchMap} from 'rxjs/operators';
+
+export function getPortsStream(ports: 'inputs'): Observable<MIDIInput[]>;
+export function getPortsStream(ports: 'outputs'): Observable<MIDIOutput[]>;
+export function getPortsStream(
+    ports: 'inputs' | 'outputs',
+): Observable<(MIDIOutput | MIDIInput)[]> {
+    return from(inject(MIDI_ACCESS).catch(() => null)).pipe(
+        switchMap(access =>
+            access
+                ? fromEvent(access as FromEventTarget<any>, 'statechange').pipe(
+                      map(() => [...access[ports].values()]),
+                      startWith([...access[ports].values()]),
+                  )
+                : of([]),
+        ),
+        shareReplay(1),
+    );
+}
