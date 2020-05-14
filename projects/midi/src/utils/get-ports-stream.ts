20927b988188a5cf68f86bbdf33e986eeb815542
waterplea <alexander@inkin.ru>
2020-05-14 23:26:59 +0300

    fix build

--- projects/midi/src/utils/get-ports-stream.ts
+++ projects/midi/src/utils/get-ports-stream.ts
@@ -2,10 +2,10 @@ import MIDIInput = WebMidi.MIDIInput;
 import MIDIOutput = WebMidi.MIDIOutput;
 
 import {inject} from '@angular/core';
-import {MIDI_ACCESS} from '@ng-web-apis/midi';
 import {from, fromEvent, Observable, of} from 'rxjs';
 import {FromEventTarget} from 'rxjs/internal/observable/fromEvent';
 import {map, shareReplay, startWith, switchMap} from 'rxjs/operators';
+import {MIDI_ACCESS} from '../tokens/midi-access';
 
 export function getPortsStream(ports: 'inputs'): Observable<MIDIInput[]>;
 export function getPortsStream(ports: 'outputs'): Observable<MIDIOutput[]>;
